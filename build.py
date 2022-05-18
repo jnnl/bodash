f560dbf17bdff58c3cbf61e9055f23ee6dba3a67
Juho Junnila <git@jnnl.eu>
2022-05-18 19:21:22 +0300

    add build script

---
+++ build.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from itertools import product
+from subprocess import call, STDOUT
+
+version = '0.2'
+progname = 'bodash'
+go_os = ['darwin', 'linux', 'windows']
+go_arch = ['amd64', 'arm64']
+
+targets = list(product(go_os, go_arch))
+for target_os, target_arch in targets:
+    outname = f'{progname}-{version}-{target_os}_{target_arch}'
+    outdir = 'dist'
+    outdir_full = f'{outdir}/{outname}'
+
+    call(['mkdir', '-vp', outdir_full], stderr=STDOUT)
+    call(f'GOOS={target_os} GOARCH={target_arch} go build -o {outdir_full}', shell=True, stderr=STDOUT)
+    if target_os == 'windows':
+        call(['zip', '-rv', f'{outname}.zip', outname], cwd=outdir, stderr=STDOUT)
+    else:
+        call(['tar', 'czvf', f'{outname}.tar.gz', outname], cwd=outdir, stderr=STDOUT)
